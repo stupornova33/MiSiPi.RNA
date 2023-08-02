afdea676c598ba24959cc7d5bbf36b7297e705ca
Chris Baugh <chris.baugh@gmail.com>
2023-08-02 12:33:38 -0400

    Create a package level documentation file
    Needed a package level documentation file to have roxygen add useDynLib to the NAMESPACE file.

---
+++ R/MiSiPi.RNA-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @docType package
+#' @name MiSiPi.RNA
+#' @useDynLib MiSiPi.RNA, .registration=TRUE
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
