a555fee5aff9e5ab65166c74d97adaeb9a634215
Taiya Jarva <t.m.jarva13@gmail.com>
2024-07-11 16:13:20 -0400

    convert arc plot to grob

--- R/dual_strand_hairpin.R
+++ R/dual_strand_hairpin.R
@@ -420,16 +420,17 @@ dual_strand_hairpin <- function(chrom_name, reg_start, reg_stop, length,
 
     minus_phasedz <- plot_hp_phasedz(minus_hp_phased_tbl, "-")
 
+    #### linux arc plot bug testing
     print("Outputting arc plot only with grDevices.")
 
     grDevices::png(file = paste0(prefix, "_grDevice_arc.png"), height = 9, width = 9, units = "in", res = 300)
-    print(arc_plot)
+    print(ggplotify::as.grob(arc_plot))
     grDevices::dev.off()
 
     print("Outputting arc plot with png()")
 
     png(paste0(prefix, "_std.png"))
-    print(arc_plot)
+    print(ggplotify::as.grob(arc_plot))
     dev.off()
 
     ## plot genome annotations (optional)
