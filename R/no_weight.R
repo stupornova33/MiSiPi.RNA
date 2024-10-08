7da6d054f75e451c5e90230e1f8e34e596b251eb
Phioria <chris.baugh@gmail.com>
2024-10-08 09:49:36 -0400

    Eliminate duplicate object creation for memory issues

--- R/no_weight.R
+++ R/no_weight.R
@@ -7,21 +7,18 @@
 #' @return filter_dt
 #' @export
 
-
 ### this is the unweighted version.
 no_weight <- function(filter_dt, chrom_name){
-   width <- pos <- start <- end <- first <- NULL
-   filter_dt$rname <- chrom_name
+  width <- pos <- start <- end <- first <- NULL
+  filter_dt$rname <- chrom_name
 
-   counts_dt <- filter_dt %>% dplyr::arrange(count)
+  expanded_dt <- filter_dt %>%
+    dplyr::arrange(count)
 
-   res <- rep_seq_reads(counts_dt$count, counts_dt$rname, counts_dt$start, counts_dt$end, counts_dt$first, counts_dt$seq)
+  expanded_dt <- rep_seq_reads(expanded_dt$count, expanded_dt$rname,
+                               expanded_dt$start, expanded_dt$end,
+                               expanded_dt$first, expanded_dt$seq) %>%
+    dplyr::mutate(width = end - start + 1)
 
-   res_df <- res[sample(1:nrow(res)), ] %>%
-     dplyr::mutate(width = end - start + 1)
-
-   res_df <- na.omit(res_df)
-
-   final_df <- res_df
-   return(final_df)
+  return(expanded_dt)
 }
