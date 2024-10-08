59cd1321c76b54bb880c0a897380a66036c203ea
Phioria <chris.baugh@gmail.com>
2024-10-08 10:17:09 -0400

    Clean up code for readability

--- R/output_readsize_dist.R
+++ R/output_readsize_dist.R
@@ -10,11 +10,11 @@
 
 output_readsize_dist <- function(dat, prefix, wkdir, strand, type){
 
-  dat <- as.data.frame(dat)
-
   empty_tbl <- data.frame(width = c(seq(18,32,1)), count = 0)
-  sizes <- merge(dat, empty_tbl, by = c("width"), all = TRUE) %>% dplyr::select(c(width, count.x)) %>% dplyr::rename("count" = "count.x") %>%
-    dplyr::select(c(count))
+  
+  sizes <- merge(dat, empty_tbl, by = c("width"), all = TRUE) %>%
+    dplyr::select(count.x) %>%
+    dplyr::rename("count" = "count.x")
 
   sizes[is.na(sizes)] <- 0
   sizes <- t(sizes)
@@ -26,12 +26,13 @@ output_readsize_dist <- function(dat, prefix, wkdir, strand, type){
   sizes <- as.data.frame(sizes)
 
   sizes$locus <- prefix
-  sizes <- sizes %>% dplyr::select(c(locus, "18", "19", "20", "21", "22", "23","24","25","26","27","28","29","30", "31", "32"))
+  sizes <- sizes %>%
+    dplyr::select(c(locus, "18", "19", "20", "21", "22", "23","24","25","26","27","28","29","30", "31", "32"))
 
-  if(!is.null(strand)){
+  if(!is.null(strand)) {
     name <- paste0(wkdir, type, "_", strand, "_read_size_distributions.txt")
     suppressWarnings(
-      if(!file.exists(name)){
+      if(!file.exists(name)) {
         utils::write.table(sizes, file = paste0(wkdir, type, "_", strand, "_read_size_distributions.txt"), sep = "\t", quote = FALSE, append = T, col.names = T, na = "NA", row.names = F)
       } else {
         utils::write.table(sizes, file = paste0(wkdir, type, "_", strand, "_read_size_distributions.txt"), quote = FALSE, sep = "\t", col.names = F, append = TRUE, na = "NA", row.names = F)
@@ -39,11 +40,10 @@ output_readsize_dist <- function(dat, prefix, wkdir, strand, type){
   } else {
       name <- paste0(wkdir,type, "_read_size_distributions.txt")
       suppressWarnings(
-        if(!file.exists(name)){
+        if(!file.exists(name)) {
           utils::write.table(sizes, file = paste0(wkdir, type, "_read_size_distributions.txt"), sep = "\t", quote = FALSE, append = T, col.names = T, na = "NA", row.names = F)
         } else {
           utils::write.table(sizes, file = paste0(wkdir, type, "_read_size_distributions.txt"), quote = FALSE, sep = "\t", col.names = F, append = TRUE, na = "NA", row.names = F)
         })
     }
-
 }
