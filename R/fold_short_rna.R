7bed1d0905b819e3375cba874c7181be2522c5b2
Phioria <chris.baugh@gmail.com>
2024-10-14 13:41:07 -0400

    Add parameters to facilitate file cleanup

--- R/fold_short_rna.R
+++ R/fold_short_rna.R
@@ -1,48 +1,61 @@
 
-fold_short_rna <- function(start, stop, converted, path_to_RNAfold){
-   write.table(converted, file = "converted.fasta", sep = "\n", append = FALSE, row.names = FALSE, quote = FALSE)
-   #fold <- system2(command = path_to_RNAfold, args = "converted.fasta", stdout= TRUE, wait = TRUE, invisible = TRUE)
+fold_short_rna <- function(start, stop, converted, path_to_RNAfold, reg_start, reg_stop, chrom_name, wkdir) {
+  write.table(converted, file = file.path(wkdir, "converted.fasta"), sep = "\n", append = FALSE, row.names = FALSE, quote = FALSE)
+  #fold <- system2(command = path_to_RNAfold, args = "converted.fasta", stdout= TRUE, wait = TRUE, invisible = TRUE)
 
-    syscheck <- unlist(unname(Sys.info()[1]))
+  syscheck <- unlist(unname(Sys.info()[1]))
 
-   if(syscheck == "Windows") {
-     fold <- system2(command = path_to_RNAfold, args = "converted.fasta", stdout= TRUE, wait = TRUE, invisible = TRUE)
-   } else if(syscheck == "Linux" | syscheck == "Darwin"){
-     fold <- system(command = path_to_RNAfold, input = converted, intern = TRUE)
-   } else {
-     print("Operating system is not Windows or Linux. Halt.")
-     return()
-   }
+  if (syscheck == "Windows") {
+    fold <- system2(command = path_to_RNAfold,
+                    args = file.path(wkdir, "converted.fasta"),
+                    stdout= TRUE,
+                    wait = TRUE,
+                    invisible = TRUE)
+  } else if (syscheck == "Linux" | syscheck == "Darwin") {
+    fold <- system(command = path_to_RNAfold, input = converted, intern = TRUE)
+  } else {
+    print("Operating system is not Windows or Linux. Halt.")
+    return()
+  }
 
-   pos_vec <- seq(2, length(fold), by = 2)
-   sep_seqs <- function(x){
-      vien_split <- stringi::stri_split_fixed(fold[2], pattern = " ")[[1]][2]
-      vien_struct <- stringi::stri_split_fixed(fold[2], pattern = " ")[[1]][1]
-      return(list(vien_split, vien_struct))
-   }
+  # Delete unwanted .ps file
+  ps_filename_to_remove <- paste(chrom_name, reg_start, reg_stop, "ss.ps", sep = "_")
+  file.remove(file.path(ps_filename_to_remove))
+  
+  pos_vec <- seq(2, length(fold), by = 2)
+  sep_seqs <- function(x) {
+    vien_split <- stringi::stri_split_fixed(fold[3], pattern = " ")[[1]][2]
+    vien_struct <- stringi::stri_split_fixed(fold[3], pattern = " ")[[1]][1]
+    return(list(vien_split, vien_struct))
+  }
 
-   test <- lapply(pos_vec, sep_seqs)
+  test <- lapply(pos_vec, sep_seqs)
 
-   make_fold <- function(x){
-      vien_struct <- test[[x]][[2]]
-      print(vien_struct)
-      vien_split <- test[[x]][[1]]
-      print(vien_split)
-      ct <- RRNA::makeCt(vien_struct, fold[[1]])
-      # Using capture.output to silence the excessive console output from ct2coord
-      utils::capture.output(coord <- RRNA::ct2coord(ct), file = nullfile())
+  make_fold <- function(x) {
+    vien_struct <- test[[x]][[2]]
+    print(vien_struct)
+    vien_split <- test[[x]][[1]]
+    print(vien_split)
+    ct <- RRNA::makeCt(vien_struct, fold[[1]])
+    # Using capture.output to silence the excessive console output from ct2coord
+    utils::capture.output(coord <- RRNA::ct2coord(ct), file = nullfile())
 
-      #split the string to get mfe
-      mfe <- gsub(' ', '', gsub('[)]','', gsub('[(]', '', vien_split)))
-      print(mfe)
-      mfe <- as.numeric(mfe)
-      start <- start
-      stop <- stop
-      converted <- converted
-      return(list(mfe, start, stop, coord, vien_struct, converted))
-   }
+    #split the string to get mfe
+    mfe <- gsub(' ', '', gsub('[)]','', gsub('[(]', '', vien_split)))
+    print(mfe)
+    mfe <- as.numeric(mfe)
+    start <- start
+    stop <- stop
+    converted <- converted
+    return(list("start" = start,
+                "stop" = stop,
+                "mfe" = mfe,
+                "vienna" = vien_struct,
+                "converted" = converted,
+                "extracted_df" = coord))
+  }
 
-   res <- lapply(1:length(test), make_fold)
+  res <- lapply(1:length(test), make_fold)
 
-   return(res)
+  return(res)
 }
