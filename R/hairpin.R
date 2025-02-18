0b2c381bf2bff1103c63f1672ac85902ff9fc700
Phioria <chris.baugh@gmail.com>
2025-02-17 21:21:04 -0500

    Remove functions from namespace

--- R/hairpin.R
+++ R/hairpin.R
@@ -1,12 +1,9 @@
-#' run the hairpin function
-#' runs hairpin function on both strands
-#' takes a gtf file, bam file, and genome file specified by user
-#' takes min_read_count specified by user. Default = 1
-#'
-#' @param vars a list
-
-#' @return plots
-#' @export
+# run the hairpin function
+# runs hairpin function on both strands
+# takes a gtf file, bam file, and genome file specified by user
+# takes min_read_count specified by user. Default = 1
+#
+# @param vars a list
 
 hairpin <- function(vars) {
   wkdir <- "hairpin_outputs/"
@@ -15,8 +12,10 @@ hairpin <- function(vars) {
   logfile <- "hairpin_logfile.txt"
   if (!file.exists(logfile) == TRUE) file.create(paste0(wkdir, logfile))
 
-  mapply(
-    dual_strand_hairpin, vars[[1]], vars[[2]], vars[[3]], vars[[4]], 1, vars[[9]], vars[[10]], logfile, wkdir, vars[[6]],
-    vars[[7]], vars[[14]], vars[[15]], vars[[18]]
+  invisible(
+    mapply(
+      dual_strand_hairpin, vars[[1]], vars[[2]], vars[[3]], vars[[4]], 1, vars[[9]], vars[[10]], logfile, wkdir, vars[[6]],
+      vars[[7]], vars[[14]], vars[[15]], vars[[18]]
+    )
   )
 }
