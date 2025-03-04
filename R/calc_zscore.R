778b58c18a0d4a548a8b692d5475c8d080ce256d
Phioria <chris.baugh@gmail.com>
2025-03-04 18:42:58 -0500

    Fix zscore calc to deal with divide by 0 and add ML zscore function

--- R/calc_zscore.R
+++ R/calc_zscore.R
@@ -1,11 +1,36 @@
-# Calculate zscore
+# calc_zscore and calc_ml_zscore
 #
-# allCounts - Data Frame
-# return z_score
+# Description: Calculates Z Scores
+# Valid Z Scores are calculated the same way in both functions
+# They differ only in how they treat scenarios where they divide by 0
+#
+# calc_zscore() assigns 0 as the Z Score if the standard devition is 0 to prevent dividing by 0
+# calc_ml_zscore() assigns -33 as the Z Score in that same situation
+#
+# Intended use:
+# calc_zscore() returns zscores used in plotting
+# calc_ml_zscore() return zscores that harshly weight divide by zero situations
+#    for use in downstream machine learning
+#
+# Parameters:
+# [counts] - A vector of integers usually passed in from as a column from a data frame
+#
+# Return: A vector of Z Scores
 
-.calc_zscore <- function(allCounts) {
-  # function to calculate z-score
+.calc_zscore <- function(counts) {
   sd <- NULL
-  z_score <- (allCounts - mean(allCounts)) / sd(allCounts)
-  return(z_score)
+  
+  if (sd(counts) == 0) {
+    return(rep(0, length(counts)))
+  }
+  return(counts - mean(counts)) / sd(counts)
+}
+
+.calc_ml_zscore <- function(counts) {
+  sd <- NULL
+  
+  if (sd(counts) == 0) {
+    return(rep(-33, length(counts)))
+  }
+  return(counts - mean(counts) / sd(counts))
 }
