02bea57b058f4b6b8935f49637731b8cf863a066
Phioria <chris.baugh@gmail.com>
2025-10-10 12:56:02 -0400

    Update logfile path

--- R/dot_siRNA.R
+++ R/dot_siRNA.R
@@ -188,10 +188,10 @@
     }
     
     if (annotate_region) {
-      log_path <- file.path(wkdir, logfile)
-      gtf_plot <- .wrapper_plot_gtf(gtf_file, chrom_name, reg_start, reg_stop, log_path)
+      #log_path <- file.path(wkdir, logfile)
+      gtf_plot <- .wrapper_plot_gtf(gtf_file, chrom_name, reg_start, reg_stop, logfile)
 
-      if(is.null(gtf_plot)){
+      if (is.null(gtf_plot)) {
         gtf_plot <- null_plot("Annotation Plot", "No features in region")
       }
       
