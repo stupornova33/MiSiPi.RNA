128b3a433b197be61df4ebcf81f5515d25f88e7f
Taiya Jarva <t.m.jarva13@gmail.com>
2026-01-25 18:09:47 -0500

    remove expport of .plot_helix function as plot_helix is locally defined in dual_strand_hairpin file.

--- R/plots.R
+++ R/plots.R
@@ -430,12 +430,12 @@
 # @param filePath a string
 # @return an arc plot
 # DEPRECATED
-.plot_helix <- function(filePath) {
-  R4RNA::plotHelix(helix = R4RNA::readHelix(filePath), line = TRUE, arrow = FALSE, lwd = 2.25, scale = FALSE)
-  grDevices::dev.control("enable")
-  temp <- grDevices::recordPlot()
-  return(temp)
-}
+#.plot_helix <- function(filePath) {
+#  R4RNA::plotHelix(helix = R4RNA::readHelix(filePath), line = TRUE, arrow = FALSE, lwd = 2.25, scale = FALSE)
+#  grDevices::dev.control("enable")
+#  temp <- grDevices::recordPlot()
+#  return(temp)
+#}
 
 .plot_siRNA_hp_phasing_probability_combined <- function(plus_phased_table, minus_phased_table) {
   phased_dist <- phased_z <- NULL
